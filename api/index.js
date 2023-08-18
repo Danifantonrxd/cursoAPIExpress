799a444ac90540a59f4f04b1f8be5461def6498a
Danifanton <danielrxd0912@gmail.com>
2023-08-18 12:40:32 -0400

    tests in main route 2

--- api/index.js
+++ api/index.js
@@ -12,11 +12,11 @@ const {
 
 const app = express();
 const port = process.env.PORT || 3010;
-const IP = "192.168.0.103";
+//const IP = "192.168.0.103";
 
 app.use(express.json());
 
-const whiteList = ["http://localhost:5500", "https://myapp.com"];
+/* const whiteList = ["http://localhost:5500", "https://myapp.com"];
 const options = {
   origin: (origin, callback) => {
     if (whiteList.includes(origin) || !origin) {
@@ -25,24 +25,24 @@ const options = {
       callback(new Error("no permitido"));
     }
   }
-}
+} */
 app.use(cors());
 
 require("./utils/auth");
 
 //----------- Main --------------//
 app.get('/', (request, response) => {
-  console.log("Nueva Peticion!!");
+  //console.log("Nueva Peticion!!");
   response.send("Hola, mi server en express")
 });
 
 app.get('/mojon', (request, response) => {
-  console.log("Nueva Peticion!!");
+  //console.log("Nueva Peticion!!");
   response.send("Hola Mojon, mi server en express")
 });
 
 app.get('/nueva-ruta', checkApiKey,(request, response) => {
-  console.log("Nueva Peticion!!");
+  //console.log("Nueva Peticion!!");
   response.send("Hola, ruta protegida")
 });
 
@@ -55,8 +55,8 @@ app.use(errorHandler);
 
 //--------------- Listen for petitions -------------------//
 app.listen(port, async () => {
-  console.log("Listening on http://" + IP + ":" + port + "/");
-  console.log(process.env.NODE_ENV);
+  //console.log("Listening on http://" + IP + ":" + port + "/");
+  //console.log(process.env.NODE_ENV);
 });
 
 
